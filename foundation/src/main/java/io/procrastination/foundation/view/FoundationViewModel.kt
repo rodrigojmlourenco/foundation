d1b4459839496e8dd8c02dbc061e1867b862320d
Rodrigo José Madeira Lourenço <rodrigo.jm.lourenco@gmail.com>
2018-08-18 10:55:00 +0100

    Chore : BaseViewModel now implements the UseCaseExcutor protocol

--- foundation/src/main/java/io/procrastination/foundation/view/FoundationViewModel.kt
+++ foundation/src/main/java/io/procrastination/foundation/view/FoundationViewModel.kt
@@ -4,12 +4,15 @@ import android.arch.lifecycle.DefaultLifecycleObserver
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import io.procrastination.foundation.domain.usecases.UseCaseExecutor
 
-open class FoundationViewModel<N : FoundationNavigator> : ViewModel(), DefaultLifecycleObserver {
-
+open class FoundationViewModel<N : FoundationNavigator> : ViewModel(), DefaultLifecycleObserver, UseCaseExecutor {
+    
     lateinit var mNavigator: N
 
-    val isLoading = MutableLiveData<Boolean>()
+    override val isLoading: MutableLiveData<Boolean>
+        get() = MutableLiveData()
+
 
     override fun onCreate(owner: LifecycleOwner) {
         isLoading.postValue(false)
